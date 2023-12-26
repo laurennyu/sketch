6d6d16272dc651aeb045808c8ac513e945bada01
laurennyu <ly39@rice.edu>
2023-12-26 02:22:52 -0500

    initial commit

---
+++ js.js
@@ -0,0 +1,12 @@
+function makeBoard(rows) {
+    const board = document.querySelector('#board');
+    // clear board
+    while(div.firstChild){
+        div.removeChild(div.firstChild);
+    }
+
+    //make board
+}
+
+// make default 16x16 board
+makeBoard(16);
