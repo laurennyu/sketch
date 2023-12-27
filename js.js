acc7789a6caca0b51be4003b5f80cfebb826c4ba
laurennyu <ly39@rice.edu>
2023-12-26 21:04:08 -0500

    build grid

--- js.js
+++ js.js
@@ -1,12 +1,23 @@
+const board = document.querySelector('#board');
 function makeBoard(rows) {
-    const board = document.querySelector('#board');
     // clear board
-    while(div.firstChild){
-        div.removeChild(div.firstChild);
+    while(board.firstChild){
+        board.removeChild(board.firstChild);
     }
-
-    //make board
+    // make board
+    for (let rowNum = 0; rowNum < rows; rowNum++) {
+        // make row
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let colNum = 0; colNum < rows; colNum++) {
+            const tile = document.createElement('div');
+            tile.classList.add('tile');
+            row.appendChild(tile);
+        }
+        board.appendChild(row);
+    }
 }
 
 // make default 16x16 board
+makeBoard(2);
 makeBoard(16);
