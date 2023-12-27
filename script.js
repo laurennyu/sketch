2d7b2b1623ac07e907dd6087d128cdf2547d4c81
laurennyu <ly39@rice.edu>
2023-12-27 18:40:25 -0500

    added image upload

--- script.js
+++ script.js
@@ -111,3 +111,17 @@ const clearBtn = document.querySelector('#clear');
 clearBtn.addEventListener('click', () =>
     makeBoard(dim)
 )
+
+// allow user to change image
+window.addEventListener('load', function() {
+    document.querySelector('input[type="file"]').addEventListener('change', function() {
+        if (this.files && this.files[0]) {
+            var img = document.querySelector('img');
+            img.onload = () => {
+                URL.revokeObjectURL(img.src);  // no longer needed, free memory
+            }
+  
+            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+        }
+    });
+  });
