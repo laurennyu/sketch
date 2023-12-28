72832c2dff31b0e4db37f1822c907dc42ebbe9b2
laurennyu <ly39@rice.edu>
2023-12-27 23:39:11 -0500

    overlayed uploaded image and drawing

--- script.js
+++ script.js
@@ -5,54 +5,6 @@ let dim = 16;
 function makePalette(colors) {
     const palette = document.querySelector('#palette');
 
-    /*let swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'red';
-    swatch.addEventListener('click', () =>
-        color = 'red'
-    )
-    palette.appendChild(swatch);
-
-    swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'orange';
-    swatch.addEventListener('click', () =>
-        color = 'orange'
-    )
-    palette.appendChild(swatch);
-
-    swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'yellow';
-    swatch.addEventListener('click', () =>
-        color = 'yellow'
-    )
-    palette.appendChild(swatch);
-
-    swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'green';
-    swatch.addEventListener('click', () =>
-        color = 'green'
-    )
-    palette.appendChild(swatch);
-
-    swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'blue';
-    swatch.addEventListener('click', () =>
-        color = 'blue'
-    )
-    palette.appendChild(swatch);
-
-    swatch = document.createElement('div');
-    swatch.classList.add('swatch');
-    swatch.style.backgroundColor = 'black';
-    swatch.addEventListener('click', () =>
-        color = 'black'
-    )
-    palette.appendChild(swatch);*/
-
     for (i in colors) {
         const swatch = document.createElement('div');
         swatch.classList.add('swatch');
@@ -96,8 +48,7 @@ function makeBoard(rows) {
 }
 
 // make palette and default board
-makePalette(['red', 'orange', 'yellow', 'blue']);
-//makePalette();
+makePalette(['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']);
 makeBoard(20);
 
 // allow user to change size of board
@@ -113,15 +64,89 @@ clearBtn.addEventListener('click', () =>
 )
 
 // allow user to change image
+
 window.addEventListener('load', function() {
     document.querySelector('input[type="file"]').addEventListener('change', function() {
         if (this.files && this.files[0]) {
-            var img = document.querySelector('img');
+            var img = document.querySelector('#preview');
             img.onload = () => {
                 URL.revokeObjectURL(img.src);  // no longer needed, free memory
             }
-  
+        
             img.src = URL.createObjectURL(this.files[0]); // set src to blob url
         }
-    });
-  });
+    })
+})
+
+
+// when image is uploaded
+/*let imgInput = document.getElementById('image-input');
+imgInput.addEventListener('change', (e) => {
+    if (e.target.files) {
+        var img = document.querySelector('img');
+        img.onload = () => {
+            URL.revokeObjectURL(img.src);  // no longer needed, free memory
+        }
+  
+        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+        
+        
+        let imageFile = e.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            var img = document.createElement("img");
+            img.onload = function (event) {
+                // Dynamically create a canvas element
+                var canvas = document.createElement("canvas");
+
+                // var canvas = document.getElementById("canvas");
+                var ctx = canvas.getContext("2d");
+
+                // Actual resizing
+                ctx.drawImage(img, 0, 0, 500, 500);
+
+                // Show resized image in preview element
+                var dataurl = canvas.toDataURL(imageFile.type);
+                document.getElementById("preview").src = dataurl;
+            }
+            img.src = e.target.result;
+        }
+        reader.readAsDataURL(imageFile);
+    }
+})
+
+
+function compressImage(originalImage, pixelationFactor) {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    //const originalWidth = originalImage.width;
+    //const originalHeight = originalImage.height;
+    const canvasWidth = originalWidth;
+    const canvasHeight = originalHeight;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    context.drawImage(originalImage, 0, 0, originalWidth, originalHeight);
+    const originalImageData = context.getImageData(
+      0,
+      0,
+      originalWidth,
+      originalHeight
+    ).data;
+    if (pixelationFactor !== 0) {
+      for (let y = 0; y < originalHeight; y += pixelationFactor) {
+        for (let x = 0; x < originalWidth; x += pixelationFactor) {
+          // extracting the position of the sample pixel
+          const pixelIndexPosition = (x + y * originalWidth) * 4;
+          // drawing a square replacing the current pixels
+          context.fillStyle = `rgba(
+            ${originalImageData[pixelIndexPosition]},
+            ${originalImageData[pixelIndexPosition + 1]},
+            ${originalImageData[pixelIndexPosition + 2]},
+            ${originalImageData[pixelIndexPosition + 3]}
+          )`;
+          context.fillRect(x, y, pixelationFactor, pixelationFactor);
+        }
+      }
+    }
+    originalImage.src = canvas.toDataURL();
+}*/
